8681bc3dc115900733a4b73135c4dfd88caf568d
Patrick Hennis <patrickhennis@gmail.com>
2015-11-23 11:38:30 -0500

    Added ability to specify target IP, username, and password through command line arguments

--- sric_script.py
+++ sric_script.py
@@ -1,8 +1,12 @@
-import time
-import socket
-import ftplib
-import os
+import time, socket, ftplib, os
 from ftplib import FTP
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("userIP", type=str, help="Target IP")
+parser.add_argument("userName", type=str, help="Target User")
+parser.add_argument("userPWD", type=str, help="Target Password")
+args = parser.parse_args()
 
 def upload(ftp, file):
     ext = os.path.splitext(file)[1]
@@ -18,9 +22,9 @@ def getFile(ftp, filename):
         print "Error"
 
 port=21
-ip=""
-password=""
-user=""
+ip = args.userIP
+user = args.userName
+password = args.userPWD
 
 while (True):
     try:
@@ -33,8 +37,9 @@ while (True):
 
         print "File List:"
         files = ftp.dir()
-
+        print "Uploading file..."
         upload(ftp, "test2.txt")
+        print "Downloading file..."
         getFile(ftp,'test.txt')
 
         print "New File List:"
